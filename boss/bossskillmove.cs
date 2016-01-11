f17652e41f9de00018975ac822bda290ee827ea1
1101137226 <ayaofkawa@gmail.com>
2016-01-11 15:12:55 +0800

    Update bossskillmove.cs

--- boss/bossskillmove.cs
+++ boss/bossskillmove.cs
@@ -1,51 +1,42 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
-public class bossskillmove : MonoBehaviour {
+public class bossskillmove : MonoBehaviour
+{
 	public Transform father;
 	float time;
-
 	public bool getout;
 	bool outflag;
-	// Use this for initialization
-	void Start () {
+
+	void Start ()
+	{
 
 		getout = false;
 		outflag = false;
 		father = GameObject.Find ("skillboss").transform;
 		this.transform.parent = father;
-		this.transform.localScale = new Vector3 (1,1,1);
+		this.transform.localScale = new Vector3 (1, 1, 1);
 		this.transform.localPosition = new Vector3 (0, 0, 0);
 		time = 0.0f;
 	}
-	
-	// Update is called once per frame
-	void Update () {
 
+	void Update ()
+	{
 		time += Time.deltaTime;
 		if (time >= 1.5f) {
-						if (!outflag) {
-								outflag = true;
-
-						}
-
-
-						this.GetComponent<UITexture> ().color = new Color (255, 255, 255, 1f - (time - 1.5f));
+			if (!outflag) {
+				outflag = true;
+			}
+			this.GetComponent<UITexture> ().color = new Color (255, 255, 255, 1f - (time - 1.5f));
 			
-						if (time >= 2.5f) {
-								Destroy (this.gameObject);
-				
-				
-						}
-				
+			if (time >= 2.5f) {
+				Destroy (this.gameObject);							
+			}				
 		
-				}
-		if (this.name == "getout") {
-				
-		
-		
-			Destroy(this.gameObject);
 		}
-	
+		if (this.name == "getout") {					
+		
+			Destroy (this.gameObject);
+		}	
 	}
 }
