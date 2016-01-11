6159d5ae54bf69729e2a567e0b4c88f561408c06
1101137226 <ayaofkawa@gmail.com>
2016-01-11 14:54:38 +0800

    Update NETDISCON.cs

--- network/NETDISCON.cs
+++ network/NETDISCON.cs
@@ -1,21 +1,18 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 using UnityEngine.Networking;
 
 public class NETDISCON : NetworkBehaviour
 {
 		public NetworkLobbyManager m;
-		// Use this for initialization
+		
 		void Start ()
 		{
 				m = GameObject.Find ("Lobby").GetComponent<NetworkLobbyManager> ();
 		}
 
 		public void OnPress (bool ispress)
-		{
-				//	manager.networkAddress = GameObject.Find ("IPadd").GetComponent<UIInput> ().value;
-		
-				//	manager.StartClient();
+		{				
 		
 				if (ispress) {
 						m.networkAddress = GameObject.Find ("IPadd").GetComponent<UIInput> ().value;
@@ -25,9 +22,9 @@ public class NETDISCON : NetworkBehaviour
 						m.StopClient ();
 				}
 		}
-		// Update is called once per frame
+		
 		void Update ()
 		{
-				//this.transform.localPosition = new Vector3 (1250, -250, 0);
+				
 		}
 }
