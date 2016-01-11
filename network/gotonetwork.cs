0430119a214ec9fa041953200512d30b54dfc738
1101137226 <ayaofkawa@gmail.com>
2016-01-11 15:06:45 +0800

    Update gotonetwork.cs

--- network/gotonetwork.cs
+++ network/gotonetwork.cs
@@ -1,4 +1,4 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 using UnityEngine.Networking;
 
@@ -9,15 +9,12 @@ public class gotonetwork : MonoBehaviour
 
 		void Awake ()
 		{
-				//manager =GameObject.Find("NET").GetComponent<NetworkManager>();
 				m = GameObject.Find ("Lobby").GetComponent<NetworkLobbyManager> ();
 				GameObject.Find ("Connect Hall").GetComponent<UIPanel> ().alpha = 1;
 				GameObject.Find ("Connect Room").GetComponent<UIPanel> ().alpha = 0;
 		}
-		// Use this for initialization
 		public void OnPress (bool ispress)
 		{
-				//manager.StartHost ();
 				if (ispress) {
 
 
@@ -25,7 +22,6 @@ public class gotonetwork : MonoBehaviour
 						GameObject.Find ("Connect Hall").GetComponent<UIPanel> ().alpha = 0;
 						GameObject.Find ("Connect Room").GetComponent<UIPanel> ().alpha = 1;
 						GameObject.Find ("Stage").GetComponent<UIPanel> ().alpha = 1;
-						//GameObject.Find ("Camera").transform.localPosition = new Vector3 (1000, 0, 0);
 				}
 		}
 }
