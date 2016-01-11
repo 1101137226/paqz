138cfddae6ea73ee8a94b543afd40cebdc002100
1101137226 <ayaofkawa@gmail.com>
2016-01-11 15:49:04 +0800

    Update screen.cs

--- screen.cs
+++ screen.cs
@@ -1,12 +1,12 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
 [RequireComponent(typeof(Camera))]
 public class screen : MonoBehaviour {
 	#region Variables
-	// 想要固定的螢幕寬度
+
 	public float m_width;
-	// 被指定的 Camera
+
 	private Camera m_camera;
 	#endregion
 	
@@ -19,9 +19,9 @@ public class screen : MonoBehaviour {
 		if( m_camera == null )
 			return ;
 		
-		// 倒著算回去
+
 		float size = (float)m_width * Screen.height / Screen.width / 2;
-		// 重新指定新的螢幕高度
+
 		m_camera.orthographicSize = size;
 
 	}
