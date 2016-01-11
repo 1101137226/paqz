47de894105d2d8b5fea6af617e26d8a0e2f674f8
1101137226 <ayaofkawa@gmail.com>
2016-01-11 15:34:51 +0800

    Update charstuas.cs

--- fight/charstuas.cs
+++ fight/charstuas.cs
@@ -1,4 +1,4 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
 public class charstuas : MonoBehaviour {
@@ -13,28 +13,19 @@ public class charstuas : MonoBehaviour {
 	public static bool issoul;
 	public static bool weakhpup = false;
 	bool isin;
-	// Use this for initialization
 	void Start () {
-	//DMG=ATK*0.8-DEF*1.5 LEVEL UP 1 point>>HP:500 atk:10 def:4 rec:5
-
-		PlayerPrefs.SetFloat ("Level",1);
-		PlayerPrefs.SetFloat ("HP",70000);
-		PlayerPrefs.SetFloat ("ATK",1200);
-		PlayerPrefs.SetFloat ("DEF",400);
-		PlayerPrefs.SetFloat ("REC",500);
+	
 		issoul = false;
 		weakhpup = false;
 		maxhp=PlayerPrefs.GetFloat("HP");
 		isin=true;
 		nowhp = maxhp;
-		Debug.Log (nowhp);
 		nowatk=PlayerPrefs.GetFloat("ATK");
 
 		nowdeff=PlayerPrefs.GetFloat("DEF");
 
 	}
 	
-	// Update is called once per frame
 	void Update () {
 		if (isin) {
 			nowhp=maxhp;
