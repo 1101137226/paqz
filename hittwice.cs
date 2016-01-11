4e4f7ff776c249e36e9bdb19d3c306de5b05eccf
1101137226 <ayaofkawa@gmail.com>
2016-01-11 15:46:21 +0800

    Update hittwice.cs

--- hittwice.cs
+++ hittwice.cs
@@ -1,14 +1,14 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
 public class hittwice : MonoBehaviour {
 	public byte hitcount;
-	// Use this for initialization
+
 	void Start () {
 		hitcount = 0;
 	}
 	
-	// Update is called once per frame
+
 	void Update () {
 	
 	}
