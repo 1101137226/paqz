933d7d5b0921910dc56efe15923dcc5dd8bc27a4
1101137226 <ayaofkawa@gmail.com>
2016-01-11 15:16:14 +0800

    Update Cardnamedisplay.cs

--- card/Cardnamedisplay.cs
+++ card/Cardnamedisplay.cs
@@ -1,15 +1,12 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
 public class Cardnamedisplay : MonoBehaviour {
-
-	// Use this for initialization
 	void Start () {
 	
 
 	}
-	
-	// Update is called once per frame
+
 	void Update () {
 
 
