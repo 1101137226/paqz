a4b83f75e4aac8f4767dd7422b254f8dec555455
1101137226 <ayaofkawa@gmail.com>
2016-01-11 15:04:43 +0800

    Update NETnewID.cs

--- network/NETnewID.cs
+++ network/NETnewID.cs
@@ -1,74 +1,72 @@
 using UnityEngine;
 using System.Collections;
 using UnityEngine.Networking;
-public class NETID : NetworkBehaviour {
+public class NETnewID : NetworkBehaviour  {
 	[SyncVar]private string playerID;
 	[SyncVar]public byte ID;
-
+	
 	NetworkInstanceId NET_ID;
 	Transform me;
 
 	public override void OnStartLocalPlayer()
 	{
-
+		
 		GetNETID();
 		SetID();
-
-
+		
+		
 	}
 	void Awake () {
 		me = transform;
 	}
 
 	void Update () {
-	if (me.name == "" || me.name == "NETCTRL(Clone)") {
-				
-		
-		
+		if (me.name == "" || me.name == "NETjk(Clone)") {
+			
+			
+			
 			SetID();
 		}
-
-
+		
+		
 	}
-
+	
 	[Client]
 	void GetNETID()
 	{
 		NET_ID = this.GetComponent<NetworkIdentity> ().netId;
-
+		
 		CmdSayMyID(myID());
-
-
+		
+		
 	}
 	void SetID()
 	{
 		if (!isLocalPlayer) {
-				
-						me.name = playerID;
-		
-		
-				} else {
-				
-		
+			
+			me.name = playerID;
+			
+			
+		} else {
+			
+			
 			me.name=myID();
-		
+			
 		}
-
+		
 	}
 	string myID()
 	{
 		string Name = "Player" + System.Convert.ToString(NET_ID);
 		return Name;
-
+		
 	}
 	[Command]
 	void CmdSayMyID(string name)
 	{
-
+		
 		playerID = name;
-
-
+		
+		
 	}
-
-
 }
