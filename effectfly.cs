86770e383666969e6e2584016dcc95cb46461a42
1101137226 <ayaofkawa@gmail.com>
2016-01-11 15:45:50 +0800

    Update effectfly.cs

--- effectfly.cs
+++ effectfly.cs
@@ -1,16 +1,16 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
 public class effectfly : MonoBehaviour {
 	public Transform father;
-	// Use this for initialization
+
 	void Start () {
 	
 
 	
 	
 		}
-	// Update is called once per frame
+
 	void Update () {
 	
 	}
