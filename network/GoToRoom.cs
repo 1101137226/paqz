073f5ec8e9693ee8bbed5ac0b251f4b8b2892612
1101137226 <ayaofkawa@gmail.com>
2016-01-11 14:51:31 +0800

    Update GoToRoom.cs

--- network/GoToRoom.cs
+++ network/GoToRoom.cs
@@ -1,24 +1,14 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
 public class GoToRoom : MonoBehaviour {
-
-	// Use this for initialization
 	void Start () {
 	
 	}
 	public void OnPress(bool ispress){
 		
-		if (ispress) {
-				
-			Application.LoadLevel("mainmemu");
-		
-		
-		
+		if (ispress) {				
+			Application.LoadLevel("mainmemu");		
 		}
-
-		
 	}
-	// Update is called once per frame
-
 }
