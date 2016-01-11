d56d15abe0eac5af36d589b73fd0d7dc3e44b86e
1101137226 <ayaofkawa@gmail.com>
2016-01-11 15:01:46 +0800

    Update NETconclick.cs

--- network/NETconclick.cs
+++ network/NETconclick.cs
@@ -1,36 +1,37 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
-public class NETconclick : MonoBehaviour {
+public class NETconclick : MonoBehaviour
+{
 	public string master;
 	public bool e;
 	UILabel o;
-	// Use this for initialization
-	void Start () {
+
+	void Start ()
+	{
 		master = GameObject.Find ("UIRoot").GetComponent<NETSAVE> ().thisname;
 		o = this.transform.GetChild (0).GetComponent<UILabel> ();
 		o.depth = 55;
 	}
-	public void OnPress(bool ispress){
+
+	public void OnPress (bool ispress)
+	{
 		
 		if (ispress) {
-			GameObject.Find(master).GetComponent<NETstaus>().CmdSet();
+			GameObject.Find (master).GetComponent<NETstaus> ().CmdSet ();
 
-			if(!e)
-			{
+			if (!e) {
 
-				o.text="Ready";
-				e=true;
-			}else if(e)
-			{
+				o.text = "Ready";
+				e = true;
+			} else if (e) {
 
-				e=false;
-				o.text="Click to Ready";
+				e = false;
+				o.text = "Click to Ready";
 		
 			}
 		
-	}
-	// Update is called once per fra
+		}	
 
-}
+	}
 }
