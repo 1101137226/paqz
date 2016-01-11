fa0a20d2f19973ca2218ecbfbff836afa6ec725b
1101137226 <ayaofkawa@gmail.com>
2016-01-11 15:07:33 +0800

    Update ishost.cs

--- network/ishost.cs
+++ network/ishost.cs
@@ -1,14 +1,11 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 using UnityEngine.Networking;
 public class ishost :NetworkBehaviour{
-
-	// Use this for initialization
 	void Start () {
 	
 	}
-	
-	// Update is called once per frame
+
 	void Update () {
 	
 	}
