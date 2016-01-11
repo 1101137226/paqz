0ce705f474c66c7210941b77a7741831a35c1903
1101137226 <ayaofkawa@gmail.com>
2016-01-11 15:00:42 +0800

    Update NETcardback.cs

--- network/NETcardback.cs
+++ network/NETcardback.cs
@@ -1,24 +1,15 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 using UnityEngine.Networking;
 public class NETcardback : NetworkBehaviour {
-
-
 	public GameObject back;
 	bool inin;
-	// Use this for initialization
+
 	void Start () {
 	
 
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
 
-
+	void Update () {	
 	}
-
-
-
 }
