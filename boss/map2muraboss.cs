67dbf82fd5dd47ff2cefcc5a58fca2d3cb8b7c7f
1101137226 <ayaofkawa@gmail.com>
2016-01-11 16:33:24 +0800

    Update map2muraboss.cs

--- boss/map2muraboss.cs
+++ boss/map2muraboss.cs
@@ -4,10 +4,10 @@ using System.Collections;
 public class map2muraboss : MonoBehaviour
 {
 	public float HP;
-	public static byte noise;
-	float maxdef = 200;
+	public static byte readonly noise;
+	float readonly maxdef = 200;
 	float def;
-	Queue defupup = new Queue ();
+	readonly Queue defupup = new Queue ();
 	public float defuptime;
 	float rec = 200;
 	public float nowhp;
