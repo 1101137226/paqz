59d20f6ec970e78e78ae25d59e209bb617364472
1101137226 <ayaofkawa@gmail.com>
2016-01-11 15:03:19 +0800

    Update NEThit.cs

--- network/NEThit.cs
+++ network/NEThit.cs
@@ -1,22 +1,15 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 using UnityEngine.Networking;
 public class NEThit : NetworkBehaviour {
-
-	// Use this for initialization
+	
 	void Start () {
 
 
 	
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
 
-	
-
-	//	GameObject.Find("Lobby").GetComponent<NetworkLobbyManager>().CheckReadyToBegin();
+	void Update () {	
 
 
 
