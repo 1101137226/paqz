9ba4dd79e76f4ea4b97f4c50e0148b14fd6e96a0
1101137226 <ayaofkawa@gmail.com>
2016-01-11 15:38:51 +0800

    Update vic.cs

--- fight/vic.cs
+++ fight/vic.cs
@@ -1,4 +1,4 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
 public class vic : MonoBehaviour {
@@ -6,7 +6,6 @@ public class vic : MonoBehaviour {
 	public byte what;
 	bool inin;
 	public bool v;
-	// Use this for initialization
 	void Start () {
 
 		this.transform.localScale=new Vector3(0.001f,0.001f,0.001f);
@@ -14,7 +13,6 @@ public class vic : MonoBehaviour {
 		inin = false;
 	}
 	
-	// Update is called once per frame
 	void Update () {
 		time += Time.deltaTime;
 
@@ -35,14 +33,29 @@ public class vic : MonoBehaviour {
 
 
 		if (time > 2f && what == 6 && v) {
+
+
+		if(Application.loadedLevelName != "VSMAP")
+			{
+
+				Application.LoadLevel ("battle_end");
+			
 				
-		
-						Application.LoadLevel ("credit");
+
+			
+			}
 		
 				} else if (time > 2f && what == 5 && !v) {
-			//Application.LoadLevel ("democardsee");
+			if(Application.loadedLevelName != "VSMAP")
+			{
+
+			if(this.name=="t")
+			{
+				Application.LoadLevel ("mainmemu");
+				
+			}
 		
-		
+			}
 		}
 	}
 }
