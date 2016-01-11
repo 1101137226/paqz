714d60054894e1e8099eb1d2af6aa435b8974690
1101137226 <ayaofkawa@gmail.com>
2016-01-11 14:59:48 +0800

    Update NETSAVE.cs

--- network/NETSAVE.cs
+++ network/NETSAVE.cs
@@ -1,16 +1,16 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
 public class NETSAVE : MonoBehaviour {
 	public string thisname;
 	public byte thisid;
 	public byte thisteam;
-	// Use this for initialization
+
 	void Start () {
 	
 	}
 	
-	// Update is called once per frame
+
 	void Update () {
 	
 	}
